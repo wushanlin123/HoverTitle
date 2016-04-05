3c5b163fa7f53508aed5c01cf05b0a75896a1681
木木 <wushanlin123@163.com>
2016-04-05 16:53:36 +0800

    代码修改

--- src/com/wusl/demo/MainActivity.java
+++ src/com/wusl/demo/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 		v = (TitleView) findViewById(R.id.true_title);
 		s = (InnerScrollView) findViewById(R.id.scrollView);
 		s.setOnScrollChangedListener(new OnScrollChangedListener() {

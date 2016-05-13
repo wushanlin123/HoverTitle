49a4c0e5b0874aaff1bde518c139e0d02f04af74
木木 <wushanlin123@163.com>
2016-05-13 14:52:10 +0800

    lll

--- src/com/wusl/demo/MainActivity.java
+++ src/com/wusl/demo/MainActivity.java
@@ -1,32 +1,20 @@
 package com.wusl.demo;
 
+import com.wusl.demo.hover.HoverFragment;
+import com.wusl.demo.scroll.ScrollFragment;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-
-import com.wusl.demo.InnerScrollView.OnScrollChangedListener;
 
 public class MainActivity extends Activity {
-	TitleView v;
-	InnerScrollView s;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		v = (TitleView) findViewById(R.id.true_title);
-		s = (InnerScrollView) findViewById(R.id.scrollView);
-		s.setOnScrollChangedListener(new OnScrollChangedListener() {
-			public void onScrollChange(int l, int t, int oldl, int oldt) {
-				int height = ((LinearLayout) s.getChildAt(0)).getChildAt(0)
-						.getHeight();
-				if (oldt >= height) {
-					v.setVisibility(View.VISIBLE);
-				} else {
-					v.setVisibility(View.GONE);
-				}
-			}
-		});
+		getFragmentManager().beginTransaction()
+				.replace(R.id.content, 
+//						new HoverFragment()
+						new ScrollFragment()
+				).commit();
 	}
 }
--- src/com/wusl/demo/hover/InnerScrollView.java
+++ src/com/wusl/demo/hover/InnerScrollView.java
@@ -1,4 +1,4 @@
-package com.wusl.demo;
+package com.wusl.demo.hover;
 
 import android.content.Context;
 import android.util.AttributeSet;
